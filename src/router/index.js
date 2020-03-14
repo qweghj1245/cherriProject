132ea451079f70916555f6fe0dc0bbaa4f9bd904
mazusu <qweghj1245@gmail.com>
2020-03-14 13:17:07 +0800

    home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '@/views/Home.vue';
+import Start from '@/views/Start.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,13 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+    children: [
+      {
+        path: '',
+        name: 'Start',
+        component: Start,
+      },
+    ],
   },
 ];
 
---
+++ src/views/Start.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex-column justify-center align-center start">
+    <img :src="require('@/assets/img/img_default.png')" alt="">
+    <div class="text-primary start-text font-bold mt-16">開始使用Cherri Chat</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.start {
+  height: calc(100% - 80px);
+  .start-text {
+    font-size: 28px;
+  }
+}
+</style>
