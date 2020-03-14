3f6b2ac6b330978d0e061529fe425fef6ca72cd1
mazusu <qweghj1245@gmail.com>
2020-03-14 16:59:27 +0800

    search

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="chat-room">
-    <div class="chat-header flex-row justify-between align-center p-16 pl-24 pr-24">
+    <div class="chat-header bg-white flex-row justify-between align-center p-16 pl-24 pr-24">
       <div class="flex-row align-center">
         <div class="head mr-8" />
         <div class="name font-bold">保羅</div>
       </div>
       <div class="flex-row">
-        <div class="icon-wrap mr-16 p-8">
+        <div class="icon-wrap mr-16 p-8" :class="{'active': showSearch}" @click="toggleShowSearch">
           <img :src="require('@/assets/img/ic_search.png')" />
         </div>
         <div
@@ -17,12 +17,12 @@
           <img :src="require('@/assets/img/ic_note.png')" />
         </div>
       </div>
-      <todo-list v-show="showTodoList"/>
+      <todo-list v-show="showTodoList" />
     </div>
-    <div class="search flex-row align-center p-16">
-      <input type="text" class="search-input p-8" />
-      <div class="about mr-24 text-grey">1則相關訊息</div>
-      <img :src="require('@/assets/img/ic_close1.png')" class="close" />
+    <div class="search flex-row align-center p-16" :class="{'show': showSearch}">
+      <input type="text" class="search-input p-8" v-model="searchText" />
+      <div class="about mr-24 text-grey">{{searchCount}}則相關訊息</div>
+      <img :src="require('@/assets/img/ic_close1.png')" class="close" @click="searchText=''" />
     </div>
     <div class="msessages flex-column justify-end align-end">
       <div
@@ -48,7 +48,30 @@ export default {
   },
   computed: {
     userMsg() {
+      let messageList = userMessage[this.activeUser].message;
+      if (this.searchText && this.searchText.length > 0) {
+        return messageList.map(item => {
+          let replaceReg = new RegExp(this.searchText, 'g');
+          let replaceString =
+            '<span class="search-text" style="background:yellow;color:black">' +
+            this.searchText +
+            '</span>';
+          return item.replace(replaceReg, replaceString);
+        });
+      }
       return this.activeUser ? userMessage[this.activeUser].message : '';
+    },
+    searchCount() {
+      let count = 0;
+      userMessage[this.activeUser].message.forEach(item => {
+        if (
+          this.searchText &&
+          this.searchText.length &&
+          item.includes(this.searchText)
+        )
+          count++;
+      });
+      return count;
     },
     activeUser() {
       return this.$store.state.activeUser;
@@ -57,11 +80,16 @@ export default {
   data() {
     return {
       showTodoList: false,
+      showSearch: false,
+      searchText: '',
     };
   },
   methods: {
     setActiveUser() {
       this.$store.commit('setActiveUser', this.$route.params.id);
+    },
+    toggleShowSearch() {
+      this.showSearch = !this.showSearch;
     },
   },
   created() {
@@ -81,6 +109,7 @@ export default {
   .chat-header {
     box-shadow: 0 4px 5px rgba(0, 0, 0, 0.1);
     position: relative;
+    z-index: 1;
     .head {
       @include size(50px, 50px);
       border-radius: 50%;
@@ -109,6 +138,13 @@ export default {
   }
   .search {
     border-bottom: solid 1px $color-light-green;
+    transform: translateY(-78px);
+    position: relative;
+    z-index: 0;
+    transition-duration: .2s;
+    &.show {
+      transform: translateY(0px);
+    }
     .search-input {
       width: 100%;
       font-size: 20px;
